e6acdb81a446dfc51ff9a3e3919d89317149eba1
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-07-12 18:08:54 +0900

    Remove unnecessary debugging messages

--- source/solvers/dpll.d
+++ source/solvers/dpll.d
@@ -4,8 +4,6 @@ import cnf, assignment;
 import std.variant : Algebraic;
 import std.typecons : Tuple;
 
-debug import std.stdio;
-
 alias Null = typeof(null);
 alias SolverResult = Algebraic!(Assignment, Null);
 
@@ -14,13 +12,10 @@ alias unitPropagateResult = Tuple!(CNF, "F", Assignment, "assignment");
 SolverResult solve(CNF F)
 {
     auto res = dpll(F, Assignment(F.variableNum));
-    debug "DPLL Done!".writeln;
     if (res.peek!Assignment)
     {
         auto tmp = res.get!0;
-        debug tmp.toDIMACSFormat.writeln;
         tmp.fillUnassignedLiterals();
-        debug tmp.toDIMACSFormat.writeln;
         res = tmp;
     }
     return res;
@@ -30,13 +25,8 @@ ulong k;
 SolverResult dpll(CNF F, Assignment assignment)
 {
     k++;
-    debug "===== %d".writefln(k);
-    debug F.writeln;
-    debug assignment.writeln;
     auto uPRes = unitPropagate(F, assignment);
     F = uPRes.F, assignment = uPRes.assignment;
-    debug F.writeln;
-    debug assignment.writeln;
 
     // if there exists an empty clause
     if (F.emptyClauses.length > 0)
@@ -56,8 +46,6 @@ SolverResult dpll(CNF F, Assignment assignment)
     if (res1.peek!Assignment)
         return SolverResult(res1);
 
-    debug "************test".writeln;
-    debug F.writeln;
     CNF F_lnotl = CNF(F);
     Assignment asgn2 = Assignment(assignment);
     F_lnotl.simplify(l.negate);
@@ -68,7 +56,6 @@ SolverResult dpll(CNF F, Assignment assignment)
 
 unitPropagateResult unitPropagate(CNF F, Assignment assignment)
 {
-    debug F.debugString.writeln;
     while (F.emptyClauses.length == 0 && F.unitClauses.length > 0)
     {
         auto k = F.unitClauses.keys[0], v = F.unitClauses[k];
@@ -76,12 +63,6 @@ unitPropagateResult unitPropagate(CNF F, Assignment assignment)
 
         F.simplify(x);
         assignment.assign(x);
-        debug "up".writeln;
-        debug F.writeln;
-        debug F.debugString.writeln;
-        debug writeln;
-
     }
-
     return unitPropagateResult(F, assignment);
 }
