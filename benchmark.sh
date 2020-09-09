46f7ceae43d00a82e41e43780c4255f3e26abef1
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-09-09 04:37:44 +0000

    add "abort" to benchmark and test

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,6 @@ bad=0
 cnt=0
 for benchdir in `cat benchmark_dir.txt`; do
 	for i in `find $benchdir -name "*.cnf"`; do
-	    echo `basename $i`,`./sat-d benchmark < $i 2>/dev/null`
+	    echo `basename $i`,`timeout 10 ./sat-d benchmark < $i 2>/dev/null || echo "ABORT,-1"`
 	done
 done
--- test.sh
+++ test.sh
@@ -4,9 +4,14 @@ cnt=0
 for i in `find testcase -name "*.cnf"`; do
     let cnt++
     echo $i
-    predict=`./sat-d < $i 2>/dev/null | head -n 1 | sed -e 's/s //'`
+    predict=`(timeout 10 ./sat-d < $i 2>/dev/null || echo ABORT) | head -n 1 | sed -e 's/s //'`
     answer=`minisat -verb=0 $i | tr -d '\n' | sed -e 's/.*precision//g'`
     
+    if test "$predict" == "ABORT"; then
+	echo "abort: $i"
+	continue
+    fi
+
     if test "$predict" != "$answer"; then
         echo "bad: $i"
         echo "predict: $predict"
