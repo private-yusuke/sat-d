1f55ed9ced0f3bfa68b05c0baf3c33d4c145c5e1
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-12-02 14:21:10 +0900

    add sudoku solver output parser

---
+++ problem_generator/sudoku_recover.d
@@ -0,0 +1,23 @@
+import std;
+
+int main(string[] args) {
+    readln; // discard "s SATISFIABLE"
+    auto lits = readln.split[1..$].to!(long[]);
+    auto maxl = lits.map!(v => v.abs).reduce!max;
+    auto n = maxl.to!real.cbrt.to!ulong;
+
+    auto board = new long[][](n, n);
+
+    foreach(lit; lits) {
+        if(lit > 0) {
+            lit--;
+            auto x = lit / (n ^^ 2), y = (lit / (n ^^ 1)) % n, z = lit % n;
+            board[x][y] = z+1;
+            // writefln("%d*%d + %d*%d + %d = %d", x, n^^2, y, n, z, lit);
+        }
+    }
+    
+    board.each!(col => writefln("%(%d %)", col));
+
+    return 0;
+}
